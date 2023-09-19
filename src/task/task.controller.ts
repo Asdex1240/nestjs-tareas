adb4b4da72c26922e8268a8e4ab6202c4aac58b8
Juan Jose Moreno Barron <jose.morenobrn@uanl.edu.mx>
2023-09-19 12:28:56 -0600

    Ya creamos y obtenemos tareas

--- src/task/task.controller.ts
+++ src/task/task.controller.ts
@@ -1,10 +1,18 @@
-import { Controller, Get } from '@nestjs/common';
-
+import { Body, Controller, Get, Post } from '@nestjs/common';
+import { TaskService } from './task.service';
+import { createTaskDTO} from './dto/task.dto'
 @Controller('task')
 export class TaskController {
 
+    constructor(private tasksService: TaskService){}
+
     @Get()
-    helloworld(){
-        return 'Hello World!'
+    getAllTasks(){
+        return this.tasksService.getAllTasks()
+    }
+
+    @Post()
+    createTask(@Body() newTask: createTaskDTO){
+        return this.tasksService.createTask(newTask.title, newTask.description);
     }
 }
--- src/task/task.service.ts
+++ src/task/task.service.ts
@@ -1,4 +1,33 @@
 import { Injectable } from '@nestjs/common';
-
+import { Task, TaskStatus } from './task.entity'
+import { v4 } from 'uuid'
 @Injectable()
-export class TaskService {}
+export class TaskService {
+
+    private tasks: Task[] = [
+        {
+            id: '1',
+            title: 'First task',
+            description: 'Some task',
+            status: TaskStatus.PENDING
+        }
+    ]
+
+    getAllTasks(){
+        return this.tasks
+    }
+
+    createTask(title: string, description: string){
+        const task = {
+            id: v4(),
+            title, 
+            description,
+            status: TaskStatus.PENDING
+        }
+        this.tasks.push(task);
+        return task
+    }
+    updateTask(){}
+    deleteTask(){}
+
+}
